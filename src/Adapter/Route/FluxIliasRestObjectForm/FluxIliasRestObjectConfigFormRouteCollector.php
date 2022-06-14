b7167834a4f9b7fb6752857cf9c9f9e9cab8f6b4
fluxfw <fluxfw@fluxlabs.ch>
2022-06-14 13:07:21 +0200

    menu icon url

--- src/Adapter/Route/FluxIliasRestObjectForm/FluxIliasRestObjectConfigFormRouteCollector.php
+++ src/Adapter/Route/FluxIliasRestObjectForm/FluxIliasRestObjectConfigFormRouteCollector.php
@@ -3,7 +3,6 @@
 namespace FluxIliasApi\Adapter\Route\FluxIliasRestObjectForm;
 
 use FluxIliasApi\Adapter\FluxIliasRestObject\FluxIliasRestObjectDto;
-use FluxIliasApi\Adapter\User\UserDto;
 use FluxIliasApi\Channel\FluxIliasRestObject\Port\FluxIliasRestObjectService;
 use FluxIliasApi\Channel\Proxy\Port\ProxyService;
 use FluxIliasApi\Libs\FluxRestApi\Adapter\Route\Collector\RouteCollector;
@@ -18,7 +17,6 @@ class FluxIliasRestObjectConfigFormRouteCollector implements RouteCollector
     private ilLocatorGUI $ilias_locator;
     private FluxIliasRestObjectDto $object;
     private ProxyService $proxy_service;
-    private UserDto $user;
 
 
     private function __construct(
@@ -26,15 +24,13 @@ class FluxIliasRestObjectConfigFormRouteCollector implements RouteCollector
         /*private readonly*/ ProxyService $proxy_service,
         /*private readonly*/ ilGlobalTemplateInterface $ilias_global_template,
         /*private readonly*/ ilLocatorGUI $ilias_locator,
-        /*private readonly*/ FluxIliasRestObjectDto $object,
-        /*private readonly*/ UserDto $user
+        /*private readonly*/ FluxIliasRestObjectDto $object
     ) {
         $this->flux_ilias_rest_object_service = $flux_ilias_rest_object_service;
         $this->proxy_service = $proxy_service;
         $this->ilias_global_template = $ilias_global_template;
         $this->ilias_locator = $ilias_locator;
         $this->object = $object;
-        $this->user = $user;
     }
 
 
@@ -43,8 +39,7 @@ class FluxIliasRestObjectConfigFormRouteCollector implements RouteCollector
         ProxyService $proxy_service,
         ilGlobalTemplateInterface $ilias_global_template,
         ilLocatorGUI $ilias_locator,
-        FluxIliasRestObjectDto $object,
-        UserDto $user
+        FluxIliasRestObjectDto $object
     ) : /*static*/ self
     {
         return new static(
@@ -52,8 +47,7 @@ class FluxIliasRestObjectConfigFormRouteCollector implements RouteCollector
             $proxy_service,
             $ilias_global_template,
             $ilias_locator,
-            $object,
-            $user
+            $object
         );
     }
 
@@ -66,8 +60,7 @@ class FluxIliasRestObjectConfigFormRouteCollector implements RouteCollector
                 $this->proxy_service,
                 $this->ilias_global_template,
                 $this->ilias_locator,
-                $this->object,
-                $this->user
+                $this->object
             ),
             GetFluxIliasRestObjectConfigFormValuesRoute::new(
                 $this->flux_ilias_rest_object_service,
